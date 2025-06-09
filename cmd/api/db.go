6298061334467ae5768e5be0eed05a99c4eef1f3
Jared Dye <jjtd2000@gmail.com>
2025-06-08 23:26:58 -0400

    remove println

--- cmd/api/db.go
+++ cmd/api/db.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"stock-talk-service/internal/models"
 )
 
 func InitTickerData() *models.TickerData {
-	fmt.Println("HERE")
 	tickerStore := &models.TickerData{}
 	return tickerStore
 }
